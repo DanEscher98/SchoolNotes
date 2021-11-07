6f6f28317bebdf8e971ca93707192a9d19d6c82a
DanEscher98 <danyiel5978@gmail.com>
2021-11-07 02:03:32 -0600

    modified:   genetic.py 	modified:   guessPasswordTests.py

--- DataScience/Genetic/genetic.py
+++ DataScience/Genetic/genetic.py
@@ -1,26 +1,39 @@
 import random
 
-def _generate_parent(length, gene_set):
+class Chromosome:
+    genes = None
+    fitness = None
+    
+    def __init__(self, genes, fitness):
+        self.genes = genes
+        self.fitness = fitness
+
+
+def _generate_parent(length, gene_set, get_fitness):
     """Generates a random guess. The loop is useful if the
     gene_set length is smaller than the target length."""
     genes = []
     while len(genes) < length:
         sample_size = min(length - len(genes), len(gene_set))
         genes.extend(random.sample(gene_set, sample_size))
-    return ''.join(genes)
+    genes = ''.join(genes)
+    fitness = get_fitness(genes)
+    return Chromosome(genes, fitness)
 
 
-def _mutate(parent, gene_set):
+def _mutate(parent, gene_set, get_fitness):
     """Change one letter. Selects two random characters, and
     a random index. If one of the chars is equal to the
     elemento at the index, then the other is selected."""
-    index = random.randrange(0, len(parent))
-    child_genes = list(parent)
+    index = random.randrange(0, len(parent.genes))
+    child_genes = list(parent.genes)
     new_gene, alternate = random.sample(gene_set, 2)
     child_genes[index] = alternate \
         if new_gene == child_genes[index] \
         else new_gene
-    return ''.join(child_genes)
+    genes = ''.join(child_genes)
+    fitness = get_fitness(genes)
+    return Chromosome(genes, fitness)
 
 
 def get_best(get_fitness, target_len,
@@ -28,18 +41,17 @@ def get_best(get_fitness, target_len,
     """Algorithm loop. Mutates until the fitness of child is
     equal or better than the optimal_fitness"""
     random.seed()
-    best_parent = _generate_parent(target_len, gene_set)
-    best_fitness = get_fitness(best_parent)
+    best_parent = _generate_parent(
+        target_len, gene_set, get_fitness)
     display(best_parent)
-    if best_fitness >= optimal_fitness:
+    if best_parent.fitness >= optimal_fitness:
         return best_parent
 
     while True:
-        child = _mutate(best_parent, gene_set)
-        child_fitness = get_fitness(child)
-        if child_fitness > best_fitness:
+        child = _mutate(best_parent, gene_set, get_fitness)
+        if child.fitness > best_parent.fitness:
             display(child)
-            if optimal_fitness > child_fitness:
-                best_fitness = child_fitness
+            if optimal_fitness > child.fitness:
+                best_parent.fitness = child.fitness
                 best_parent = child
             else: return child # Success!
--- DataScience/Genetic/guessPasswordTests.py
+++ DataScience/Genetic/guessPasswordTests.py
@@ -4,14 +4,17 @@ import genetic
 
 
 class guessPasswordTests(unittest.TestCase):
-    gene_set = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!."
+    lower_case  = "abcdefghijklmnñopqrstuvwxyz"
+    upper_case  = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+    numbers     = "0123456789"
+    ascii_chars = " ,.!?@_-"
+    gene_set = lower_case + upper_case + \
+        numbers + ascii_chars
     
     def test_Hello_World(self):
-        target = "Hello World!"
+        target = "Hello World! Today is a sunny day."
         best = guess_password(target, self.gene_set)
-        self.assertEqual(best, target)
-
-    
+        self.assertEqual(best.genes, target)
 
 
 def get_fitness(guess, target):
@@ -20,20 +23,22 @@ def get_fitness(guess, target):
                if expected == actual )
 
 
-def display(guess, target, start_t):
+def display(candidate, target, start_t):
     time_diff = datetime.datetime.now() - start_t
-    fitness = get_fitness(guess, target)
-    print("{0}\t{1}\t{2}".format(guess, fitness, str(time_diff)))
+    print("{0}\t{1}\t{2}".format(
+        candidate.genes, candidate.fitness, str(time_diff)))
 
 
 def guess_password(target, gene_set):
     start_t = datetime.datetime.now()
     optimal_fitness = len(target)
-    return genetic.get_best(lambda guess: get_fitness(guess, target),
-                     optimal_fitness,
-                     optimal_fitness,
-                     gene_set,
-                     lambda guess: display(guess, target, start_t))
+    return genetic.get_best(
+        lambda guess: get_fitness(guess, target),
+        optimal_fitness,
+        optimal_fitness,
+        gene_set,
+        lambda guess: display(guess, target, start_t))
+
 
 if __name__=='__main__':
     unittest.main()
